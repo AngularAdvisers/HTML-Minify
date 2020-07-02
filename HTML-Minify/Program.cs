4d9f969cbb92bbd6f980903a0cad0505bd0f5db5
Angular Advisors <AngularAdvisors@gmail.com>
2020-07-02 14:11:39 -0600

    Added source code

---
+++ HTML-Minify/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace HTML_Minify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("A file name must be specified");
+            }
+            else
+            {
+                Console.WriteLine("This has {0} arguments, the arguments are: ", args.Length);
+                for (int i = 0; i < args.Length; ++i) Console.WriteLine(args[i]);
+                var filename = args[0];
+                string line = File.ReadAllText(filename);
+                var minline = line.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+                File.WriteAllText("min-" + filename, minline);
+            }
+        }
+    }
+}
