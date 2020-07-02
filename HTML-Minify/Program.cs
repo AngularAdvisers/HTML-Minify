d2dc550489638d31e037140f89bd2dabddac936d
Angular Advisors <AngularAdvisors@gmail.com>
2020-07-02 14:54:49 -0600

    Added html minify

--- HTML-Minify/Program.cs
+++ HTML-Minify/Program.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HTML_Minify
 {
+    public static class HtmlMinifier
+    {
+        private const string URL_HTML_MINIFIER = "https://html-minifier.com/raw";
+        private const string POST_PAREMETER_NAME = "input";
+
+        public static async Task<String> MinifyHtml(string inputHtml)
+        {
+            List<KeyValuePair<String, String>> contentData = new List<KeyValuePair<String, String>>
+        {
+            new KeyValuePair<String, String>(POST_PAREMETER_NAME, inputHtml)
+        };
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (FormUrlEncodedContent content = new FormUrlEncodedContent(contentData))
+                {
+                    using (HttpResponseMessage response = await httpClient.PostAsync(URL_HTML_MINIFIER, content))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -17,8 +46,14 @@ namespace HTML_Minify
                 for (int i = 0; i < args.Length; ++i) Console.WriteLine(args[i]);
                 var filename = args[0];
                 string line = File.ReadAllText(filename);
-                var minline = line.Replace("\n", "").Replace("\r", "").Replace(" ", "");
-                File.WriteAllText("min-" + filename, minline);
+                string minline = "";
+                using (Task<String> task = HtmlMinifier.MinifyHtml(line))
+                {
+                    task.Wait();
+                    minline = task.Result; // ...
+                }
+                minline = "\"" + minline.Replace("\"", "\\\"") + "\"";
+                File.WriteAllText(filename + ".min", minline);
             }
         }
     }
